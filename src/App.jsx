98af5a9c09f30223c03c73d4f0372dae10f6e764
Theron <theron.halsey@gmail.com>
2023-11-18 16:43:39 -0500

    test pet call

--- src/App.jsx
+++ src/App.jsx
@@ -10,11 +10,14 @@ function App() {
   const [pets, setPets] = useState([]);
   const [currentPetIndex, setCurrentPetIndex] = useState(0);
   const [currentPage, setCurrentPage] = useState(1);
-  //const [userPreferences, setUserPreferences] = useState([]);
+  const [userPreferences, setUserPreferences] = useState([]);
 
   useEffect(() => {
     fetch(`/Petfinder/getPets/${currentPage}`)
-      .then(res => res.json())
+      .then(res => {
+        const resClone = res.clone();
+        console.log(resClone.text())
+        return res.json()})
       .then(data => setPets(data))
   }, [currentPage]);
 
@@ -30,19 +33,19 @@ function App() {
   };
 
   const handleDislike = () => {
-    //setUserPreferences([...userPreferences, { id: pets[currentPetIndex].id, preference: 'dislike' }])
+    setUserPreferences([...userPreferences, { id: pets[currentPetIndex].id, preference: 'dislike' }])
     nextPet();
   }
   const handleLike = () => {
-    //setUserPreferences([...userPreferences, { id: pets[currentPetIndex].id, preference: 'like' }])
+    setUserPreferences([...userPreferences, { id: pets[currentPetIndex].id, preference: 'like' }])
     nextPet();
   }
-/*
+
   //console log userPreferences
   useEffect(() => {
     console.log(userPreferences)
   }, [userPreferences]) //only runs when userPreferences changes, console log userPreferences
-*/
+
 
   return (
     <>
