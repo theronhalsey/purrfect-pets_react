e1eee6fe5a55a4793b0c71321df6ed6e0e239bc4
Theron <theron.halsey@gmail.com>
2023-12-05 19:30:59 -0500

    test deploy

---
+++ utils/db/config.js
@@ -0,0 +1,19 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+const server = process.env.NODE_AZURE_SQL_SERVER;
+const database = process.env.NODE_AZURE_SQL_DATABASE;
+const port = parseInt(process.env.NODE_AZURE_SQL_PORT);
+const user = process.env.NODE_AZURE_SQL_USER;
+const password = process.env.NODE_AZURE_SQL_PASSWORD;
+
+export const config = {
+    server,
+    port,
+    database,
+    user,
+    password,
+    options: {
+        encrypt: true
+    }
+};
